7144047afa13772249e89d7b3b524648abd5f3c2
Carlos84sh <Cs.huelves@gmail.com>
2020-12-03 19:19:24 +0000

    Subi mitad la base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -2,12 +2,40 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=db.datetime.datebimne.utcnow, nullable=False)
+    update_at = db.Column(db.DateTime, default=db.datetime.datebimne.utcnow, nullable=False)
+    delete_at = db.Column(db.DateTime, default=db.datetime.datebimne.utcnow, nullable=False)
+    first_name = db.Column(db.String(50)
+    last_name = db.Column(db.String(50)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    country = db.Column(db.String(50)
+    language = db.Column(db.String(50)
+    avatar = db.Column(db.String(50)
 
+
+class cities(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=db.datetime.datebimne.utcnow, nullable=False)
+    update_at = db.Column(db.DateTime, default=db.datetime.datebimne.utcnow, nullable=False)
+    delete_at = db.Column(db.DateTime, default=db.datetime.datebimne.utcnow, nullable=False)
+    name = db.Column(db.String(50)
+    image = db.Column(db.String(150)
+    population = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+    cost_of_living = db.Column(db.Integer)
+    sunny = db.Column(db.Integer)
+    humidity = db.Column(db.Integer)
+    windy = db.Column(db.Integer)
+    rainy = db.Column(db.Integer)
+    lowest_temp = db.Column(db.float)
+    highest_temp =db.Column(db.float)
+    average_temp = db.Column(db.float)
+    rental_offer = db.Column(db.Integer)
+       
+ 
     def __repr__(self):
         return '<User %r>' % self.username
 
