b6a602cfea4a7fdfc1bb18e0c2ba5c5d7cbf068b
Carlos84sh <Cs.huelves@gmail.com>
2021-01-11 19:23:37 +0000

    component card

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,12 +4,13 @@ import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
 import { Demo } from "./views/registrer";
-import { Single } from "./views/single";
+
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 import { MyCarousel } from "./component/images";
+import { Cards } from "./component/card";
+import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
@@ -25,14 +26,13 @@ const Layout = () => {
 					<MyCarousel />
 					<Switch>
 						<Route exact path="/">
+							<Cards />
 							<Home />
 						</Route>
 						<Route exact path="/demo">
 							<Demo />
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
-						</Route>
+						<Route exact path="/single/:theid" />
 						<Route>
 							<h1>Not found!</h1>
 						</Route>
