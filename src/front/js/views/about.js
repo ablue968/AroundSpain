bd6ca4f46c943aaa567c4e968d3a961993b80939
ablue968 <ablue968@gmail.com>
2021-01-12 20:15:06 +0000

    finalizadas las vistas, sirven los links para cada vista

--- src/front/js/views/about.js
+++ src/front/js/views/about.js
@@ -1,1 +1,24 @@
-import React, { useContext } from "react";
+import React from "react";
+
+export const AboutUs = () => {
+	return (
+		<main>
+			<div className="d-flex justify-content-center">
+				<h4>About Us!!</h4>
+			</div>
+			<div>
+				<div className="d-flex justify-content-center">
+					<img src="https://picsum.photos/id/1016/480/250" />
+				</div>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+					industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+					and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+					leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+					with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+					publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				</p>
+			</div>
+		</main>
+	);
+};
---
+++ src/front/js/views/city.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+
+export const CityPage = () => {
+	const params = useParams();
+	const [like, setLike] = useState("far fa-heart text-danger");
+
+	const handleClick = () => {
+		setLike("fas fa-heart text-danger");
+	};
+	return (
+		<div className="container">
+			<div className="row align-items-center">
+				<div className="col-2 text-center">
+					<img src="https://picsum.photos/900/500" />
+					<button type="btn" onClick={handleClick}>
+						<i className={like} />
+					</button>
+				</div>
+				<div className="col-6 text-center pr-5">
+					<h1>
+						{params.id}
+						.cityName
+					</h1>
+				</div>
+			</div>
+
+			<div className="row">
+				<div className="col-2 text-center ">
+					<h5>Caracteristica 1</h5>
+					<p>
+						city.Caracteristica
+						{params.id}
+					</p>
+				</div>
+				<div className="col-2 text-center ">
+					<h5>Caracteristica 2</h5>
+					<p>
+						city.Caracteristica
+						{params.id}
+					</p>
+				</div>
+				<div className="col-2 text-center">
+					<h5>Caracteristica 3</h5>
+					<p>
+						city.Caracteristica
+						{params.id}
+					</p>
+				</div>
+				<div className="col-2 text-center ">
+					<h5>Caracteristica 4</h5>
+					<p>
+						city.Caracteristica
+						{params.id}
+					</p>
+				</div>
+				<div className="col-2 text-center">
+					<h5>Caracteristica 5</h5>
+					<p>
+						city.Caracteristica
+						{params.id}
+					</p>
+				</div>
+				<div className="col-2 text-center">
+					<h5>Caracteristica 6</h5>
+					<p>
+						city.Caracteristica
+						{params.id}
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
