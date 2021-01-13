d9243e9ec0d39ff9c542290e57b786dd4cfb78ce
ablue968 <ablue968@gmail.com>
2021-01-13 16:54:09 +0000

    ahora sí, todas las vistas

--- src/front/js/views/home.js
+++ src/front/js/views/home.js
@@ -1,25 +1,88 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.scss";
 
 export const Home = () => {
-	// buscar en cuaderno para usar el "conditional template for cards" habría que abrir en components un map de estas cards
 	const { store, actions } = useContext(Context);
 
+	// useEffect(()=>{
+	//     actions.getPlanets();
+	// },[]) que pasa si yo pongo [store]?
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
+		<>
+			<div className="container-fluid d-flex justify-content-center row mb-3">
+				<h2 className="col-12 text-center">Los más buscados</h2>
+				<div
+					className="card col-2"
+					style={{ width: "18rem", marginLeft: "2rem", marginTop: "2rem", padding: "0px" }}>
+					<img src="https://picsum.photos/id/267/200/200" className="card-img-top" alt="..." />
+				</div>
+
+				<div
+					className="card col-2"
+					style={{ width: "18rem", marginLeft: "2rem", marginTop: "2rem", padding: "0px" }}>
+					<img src="https://picsum.photos/id/277/200/200" className="card-img-top" alt="..." />
+				</div>
+				<div
+					className="card col-2"
+					style={{ width: "18rem", marginLeft: "2rem", marginTop: "2rem", padding: "0px" }}>
+					<img src="https://picsum.photos/id/267/200/200" className="card-img-top" alt="..." />
+				</div>
+
+				<div
+					className="card col-2"
+					style={{ width: "18rem", marginLeft: "2rem", marginTop: "2rem", padding: "0px" }}>
+					<img src="https://picsum.photos/id/277/200/200" className="card-img-top" alt="..." />
+				</div>
+			</div>
+			<h2 className="text-center">Elige el próximo destino</h2>
+			<div className="container row">
+				<div
+					className="card col-2"
+					style={{ width: "18rem", marginLeft: "2rem", marginTop: "2rem", padding: "0px" }}>
+					<img src="https://picsum.photos/id/267/200/200" className="card-img-top" alt="..." />
+				</div>
+
+				<div
+					className="card col-2"
+					style={{ width: "18rem", marginLeft: "2rem", marginTop: "2rem", padding: "0px" }}>
+					<img src="https://picsum.photos/id/277/200/200" className="card-img-top" alt="..." />
+				</div>
+				<div
+					className="card col-2"
+					style={{ width: "18rem", marginLeft: "2rem", marginTop: "2rem", padding: "0px" }}>
+					<img src="https://picsum.photos/id/267/200/200" className="card-img-top" alt="..." />
+				</div>
+
+				<div
+					className="card col-2"
+					style={{ width: "18rem", marginLeft: "2rem", marginTop: "2rem", padding: "0px" }}>
+					<img src="https://picsum.photos/id/277/200/200" className="card-img-top" alt="..." />
+				</div>
+			</div>
+		</>
 	);
 };
+
+{
+	/* <div className="peopleDiv row flex-row flex-nowrap">
+                {store.planets.map((planet, index) =>
+                    {const {name , climate, population} = planet;
+                    return(
+                        <div key={index} className="card col-3">
+                        <img src="http://via.placeholder.com/400x200" className="card-img-top" alt="..."></img>
+                        <div className="card-body">
+                            <p>Name :{name}</p>
+                            <p>Climate :{climate}</p>
+                            <p>Population: {population}</p>
+                            <div className="d-flex justify-content-between">
+                                <button className="btn btn-outline-primary">Learn more!</button>
+                                <button className="btn btn-outline-warning">♡</button>
+                            </div>
+                        </div>
+                    </div>
+                    )}
+                )}
+
+            </div> */
+}
