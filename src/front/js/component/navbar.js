c37359855173073fd74d4b5b5e8cbb8ebfd6c082
ablue968 <ablue968@gmail.com>
2021-01-14 14:18:54 +0000

    cambios 14/01

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,40 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+		<nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark d-flex justify-content-around">
 			<div className="container-fluid row">
 				<Link to="/">
-					<a className="navbar-brand col-2" href="#">
-						¿ÑOMADA?
-					</a>
+					<h4>¿ÑOMADA?</h4>
 				</Link>
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0" />
-					<form className="d-flex col-8">
+					<form className="d-flex col-8 ml-4">
 						<input className="form-control" type="search" placeholder="Ciudad" aria-label="Search" />
 						<button className="btn btn-outline-danger" type="submit">
 							Search
 						</button>
 					</form>
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0 col-2">
-						<li className="nav-item">
-							<Link to="/about">
-								<a className="nav-link" href="#">
-									ABOUT US
-								</a>
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/login">
-								<a className="nav-link" href="#" aria-disabled="true">
-									LOGIN
-								</a>
-							</Link>
-						</li>
-					</ul>
 				</div>
+				<Dropdown>
+					<Dropdown.Toggle variant="danger" id="dropdown-basic">
+						<i className="fas fa-heart" />
+						{/* <i className="far fa-heart" /> */}
+					</Dropdown.Toggle>
+					<Dropdown.Menu>
+						<Dropdown.Item eventKey="1">Action</Dropdown.Item>
+						<Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+						<Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+						<Dropdown.Divider />
+						<Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
+				<Link to="/about">
+					<button type="btn" className="btn btn-info d-flex ml-2">
+						ABOUT US
+					</button>
+				</Link>
+				<Link to="/login">
+					<button type="btn" className="btn btn-success ml-2">
+						LOGIN
+					</button>
+				</Link>
 			</div>
 		</nav>
 	);
--- src/front/js/views/login.js
+++ src/front/js/views/login.js
@@ -19,22 +19,30 @@ export const Login = () => {
 	};
 
 	return (
-		<main className="d-flex justify-content-center row">
+		<main className="d-flex justify-content-center row mt-5">
 			<div className="col-2">
-				<h1 className="d-flex justify-content-center">Login</h1>
-				<br />
-				<h2>Username</h2>
-				<input value={username} type="text" onChange={() => setUsername(event.target.value)} />
-				<h2>Password</h2>
-				<input value={password} type="password" onChange={() => setPassword(event.target.value)} />
-				<div className="form-group form-check">
+				<h1 className="d-flex justify-content-center mb-3">Login</h1>
+				<div className="text-center">
+					<h2>Username</h2>
+					<input value={username} type="text" onChange={() => setUsername(event.target.value)} />
+				</div>
+				<div className="text-center">
+					<h2>Password</h2>
+					<input
+						value={password}
+						type="password"
+						width="100%"
+						onChange={() => setPassword(event.target.value)}
+					/>
+				</div>
+				<div className="form-group form-check mt-3">
 					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
 					<label className="form-check-label" htmlFor="exampleCheck1">
 						Remember my user!
 					</label>
 				</div>
 
-				<div className="d-flex justify-content-center">
+				<div className="d-flex justify-content-center mt-2">
 					<Link to="/">
 						<button className="btn btn-primary">Back home</button>
 					</Link>
@@ -43,6 +51,11 @@ export const Login = () => {
 						Submit
 					</button>
 				</div>
+				<Link to="/register">
+					<div className="mt-4 text-center">
+						<h5>Come and register!!</h5>
+					</div>
+				</Link>
 			</div>
 		</main>
 	);
