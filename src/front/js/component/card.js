c67db82c64e1b8538d1a634e4f8996cc822ac113
ablue968 <ablue968@gmail.com>
2021-01-23 20:21:45 +0000

    quedan muchas cosas por hacer

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -1,16 +1,25 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, Fragment } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { Context } from "../store/appContext";
 
 import "../../styles/all.scss";
+import "../../styles/cards.scss";
+
+{
+	/*no está funcionando bien la card, no veo muy bien como resolverlo */
+}
 
 export const Card = props => {
 	const { store, actions } = useContext(Context);
 
 	const { city } = props;
 
+	const [background, setBackgroung] = useState("card-img-top");
+	const [showCardText, setShowCardText] = useState(false);
+
 	const [like, setLike] = useState("far fa-heart text-danger");
+
 	const handleClick = event => {
 		if (like == "far fa-heart text-danger") {
 			setLike("fas fa-heart text-danger");
@@ -21,25 +30,41 @@ export const Card = props => {
 		}
 	};
 
+	const setCardBackground = (effect, text) => {
+		setBackgroung(effect);
+		setShowCardText(text);
+	};
+
 	return (
 		<div className="card d-flex flex-column topCityCard">
-			<Link
-				to={`/city/${city.city_name}`}
-				onMouseOver={() => {
-					console.log("info funciton");
-				}}>
-				<img src={city.image} className="card-img-top" alt={city.city_name} />
+			<Link to={`/city/${city.city_name}`}>
+				<img
+					src={city.image}
+					className={background}
+					alt={city.city_name}
+					onMouseEnter={() => setCardBackground("card-img-top toBlur", true)}
+					onMouseOut={() => setCardBackground("card-img-top", false)}
+				/>
 			</Link>
-			<div className="card-img-overlay d-flex flex-column  pt-2">
-				<p className="text-right" id="lobster">
-					{city.city_name}
-				</p>
-			</div>
-			<div className="card-img-overlay d-flex flex-column  pt-2">
-				<button id="likeButton" onClick={() => handleClick(event)}>
-					<i className={like} title={city.city_name} />
-				</button>
-			</div>
+			{showCardText ? (
+				<React.Fragment className=" d-flex card-img-overlay">
+					<h5
+						className="text-right d-flex "
+						id="lobster"
+						onMouseEnter={() => setCardBackground("card-img-top toBlur", true)}>
+						{city.city_name}
+					</h5>
+					<h5
+						className="d-flex btn-block"
+						id="likeButton"
+						onClick={() => handleClick(event)}
+						onMouseOut={() => setCardBackground("card-img-top", false)}>
+						<i className={like} title={city.city_name} />
+					</h5>
+				</React.Fragment>
+			) : (
+				""
+			)}
 		</div>
 	);
 };
