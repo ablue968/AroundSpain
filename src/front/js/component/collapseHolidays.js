4e9df819271446b40acfb9a79d0d77dfb6198018
ablue968 <ablue968@gmail.com>
2021-04-14 18:23:57 +0000

    need to better the getHoliday

--- src/front/js/component/collapseHolidays.js
+++ src/front/js/component/collapseHolidays.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useContext} from "react";
+import { Context } from "../store/appContext";
 
 
 export const collapseHolidays = () => {
+
+    const { store, actions } = useContext(Context);
+
+
     return(
         <div> acá va la info de las fiestas</div>
     )
